1b43390df98ac7ee4ed31b87f11bb747558a3e3e
Muhammed Hazim Abhar bin Mohamed Azahar <hazimabhar123@gmail.com>
2023-06-03 20:54:26 +0800

    update

--- src/views/CashierView.vue
+++ src/views/CashierView.vue
@@ -24,8 +24,8 @@ document.title="Cashier"
                         <span>Fungsi Ini Hanya Boleh Digunakan Pada Kumputer Sahaja</span> 
                     </p>
                 </div>
-                <div class="flex max-lg:hidden">
-                    <div class=" w-3/5 mx-9">
+                <div class="flex max-lg:hidden min-h-[300px]">
+                    <div class="h-fit w-3/5 mx-9">
                     <div>
                         <table>
                             <thead>
@@ -42,7 +42,7 @@ document.title="Cashier"
                                     <td class="w-[60%] text-slate-500 border-b-2 font-medium text-center" >{{ item.name }}</td>
                                     <td class="w-[5%] text-slate-500 border-b-2 font-medium text-center select-none"> <i class="fa-solid fa-plus text-blue-500 font-medium text-sm cursor-pointer text-center" @click="addQuantity(item)"></i> {{ item.quantity }} <i class="fa-solid fa-minus text-sm text-red-500 cursor-pointer" @click="decreaseQuantity(item)"></i></td>
                                     <td class="text-slate-500 border-b-2 font-medium text-end" >RM {{ item.price * item.quantity }}</td>
-                                    <td><i class="fa-solid fa-trash text-[#ff0000] cursor-pointer ml-5"></i></td>
+                                    <td><i class="fa-solid fa-trash text-[#ff0000] cursor-pointer ml-5" @click="deleteItem(index)"></i></td>
                                 </tr>
                             </tbody>
                         </table>
@@ -50,33 +50,33 @@ document.title="Cashier"
                         <br>
                         <form @submit.prevent="scanBarcode" autocomplete="off" >
                             <div class="bg-white shadow-product p-5 rounded-2xl border ">
-                                <input ref="barkodProduk" v-model="barkodProduk" class="outline-none" type="text" placeholder="Imbas Kod Bar" id="kodbar">
+                                <input ref="barkodProduk" v-model="barkodProduk" class="w-full outline-none" type="text" placeholder="Imbas Kod Bar" id="kodbar">
                             </div>
                         </form>
                     </div>
                     <div class="w-3/12 h-fit bg-white shadow-product p-4 rounded-xl fixed right-20">
                         <div class="flex justify-between p-2">
                             <p>Barang:</p>
-                            <p>8</p>
+                            <p>{{ totalQuantity }}</p>
                         </div>
                         <div class="flex justify-between p-2">
                             <p>Jumlah Harga:</p>
-                            <p>8.88</p> 
+                            <p>RM {{ totalPrice.toFixed(2) }}</p> 
                         </div>
                         <hr>
                         <div class="p-2">
                             <p class="text-center">Jenis Pembayaran</p>
                             <div class="p-2">
-                                <input type="radio" name="jenispembayaran" id="jenispembayaran" value="Tunai">
+                                <input type="radio" name="jenispembayaran" id="jenispembayaran" value="Tunai" v-model="paymentType">
                                 <label for="Tunai">Tunai</label>
                             </div>
                             <div class="p-2">
-                                <input type="radio" name="jenispembayaran" id="jenispembayaran" value="DalamTalian">
+                                <input type="radio" name="jenispembayaran" id="jenispembayaran" value="Online" v-model="paymentType">
                                 <label for="DalamTalian">Dalam Talian</label>
                             </div>
                         </div>
                         <div class="w-max mx-auto">
-                            <MyButton txt="Bayar"/>
+                            <MyButton txt="Bayar" @click="completeSale"/>
                         </div>
                     </div>
                 </div>
@@ -96,12 +96,29 @@ export default
         barkodProduk: '',
         itemDetail:'',
         exisitngItem:null,
-        listItem:[]
+        totalSale:null,
+        listItem:[],
+        sale:[],
+        paymentType:''
         }
     } ,
     mounted()
     {
         this.$refs.barkodProduk.focus()
+    },
+    computed:{
+        totalQuantity()
+        {
+            return this.listItem.reduce((total, item)=> total + item.quantity,0)
+        },
+        totalPrice()
+        {
+           return this.listItem.reduce((total, item)=> total+(item.price*item.quantity),0)
+        },
+        idItems()
+        {
+            return this.listItem.map(item=>item.idItem)
+        }
     },
     methods:
     {
@@ -112,7 +129,6 @@ export default
             .then(response=>{
                 this.itemDetail = response.data
                 console.log(this.itemDetail)
-                console.log(this.listItem)
                 this.barkodProduk=''
 
                 this.existingItem = this.listItem.find(item=> item.idItem === this.itemDetail.idItem)
@@ -126,10 +142,11 @@ export default
                     this.listItem.push({...this.itemDetail,quantity:1})
 
                 }
-
                 window.scrollTo({
                 top: document.body.scrollHeight,
                 behavior: 'smooth' // Optional: Add smooth scrolling animation
+
+                
             })
             })
             .catch(error=>console.log(error))
@@ -143,10 +160,61 @@ export default
             if(item.quantity>1)
             {
                 item.quantity--
+            }else
+            {
+                const index = this.listItem.indexOf(item);
+                if (index !== -1) {
+                this.listItem.splice(index, 1);
+                }
             }
+        },
+        async deleteItem(index)
+        {
+            this.listItem.splice(index, 1);
+        },
+        async completeSale()
+        {
+            console.log(this.listItem)
+            console.log(this.totalPrice)
+            console.log(this.totalQuantity)
+            console.log(this.paymentType)
+            console.log(this.idItems)
+
+            this.sale.push([...this.listItem]),
+            this.listItem=[],
+            console.log(this.sale)
+            console.log(this.listItem)
+
+            console.log(this.sale.length)
+
+            this.totalSale= this.sale.reduce((total,outerArray)=> {
+                outerArray.forEach(item => {
+                  const itemPrice = item.price*item.quantity
+                  total += itemPrice  
+                })
+                return total
+            },0)
+            console.log(this.totalSale)
+
+            // const data={
+            //     items:this.idItems
+            // }
+            // console.log(data)
+
+            // await axios.post("http://localhost:3000/buylist",data)
+            // .then(response=>console.log(response))
+            // .catch(error=>console.log(error))
+
+
+            const report ={
+                numberSale:this.sale.length,
+                saleRevenue:this.totalSale
+            }
+
+            await axios.post("http://localhost:3000/report",report)
+            .then(response=>console.log(response))
+            .catch(error=>console.log(error))
         }
-    
-        
     }
 }
 
