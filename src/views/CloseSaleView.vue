0278a84d9f7dccf35dbd4532940c30427db3fe92
Muhammed Hazim Abhar bin Mohamed Azahar <hazimabhar123@gmail.com>
2023-06-10 02:56:23 +0800

    update

--- src/views/CloseSaleView.vue
+++ src/views/CloseSaleView.vue
@@ -22,12 +22,69 @@ document.title="Tutup Jualan"
                     <span class="px-2">></span>
                     <li>Tutup Jualan</li>
                 </ul>
-                <div class="text-center relative">
-                    <button class=" bg-black text-white p-5 rounded-2xl hover:bg-red-600 hover:scale-125" @click="toggleDialog()" >Tutup Jualan</button>
+                <div class=" flex text-center gap-5 justify-center items-center">
+                    <div>
+                        <button class=" bg-black text-white p-5 px-14 rounded-2xl hover:bg-red-600 hover:scale-105" @click="toggleDialog()" >Tutup Jualan</button>
+                    </div>
+                    <div>
+                        <button class=" bg-black text-white p-5 rounded-2xl hover:bg-red-600 hover:scale-105"  :class="[tutupJualan ? '' : 'opacity-50 cursor-not-allowed']" :disabled="!tutupJualan" @click="downloadReport">Muat Turun Laporan</button>
+                    </div>
+                </div>
+                <div class="mt-10" id="Report" >
+                    <div class="flex justify-center items-center">
+                        <h1 class="text-center text-2xl text-blue-600 font-bold">Laporan Jualan Harian</h1> 
+                    </div>
+                    <div class="flex justify-between w-[69%] mx-auto my-5">
+                        <p class="font-bold text-sm">Jumlah Jualan: {{ allSale.length }}</p>
+                        <p class="font-bold text-sm ">Hasil Jualan: RM {{ total }}</p>
+                        <h2 class="font-bold text-sm" >Tarikh: {{ currentDate }}</h2>
+                    </div>
+                    <div class="pb-5" v-for="(sale, index) in allSaleReport" :key="index">
+                        <div class="flex justify-between w-9/12 mx-auto bg-[#D9D9D9] p-3 px-10 rounded-3xl text-sm">
+                            <p class="font-semibold">Number Sale: {{ index+1 }}</p>
+                            <p class="font-semibold ml-16">Total Sale: RM {{ sale.price }}</p>
+                            <p class="font-semibold" >Worker Incharge: {{ sale.workerName }}</p>
+                        </div>
+                        <div class="flex justify-between w-9/12 mx-auto p-3 px-10 text-sm">
+                            <p class="font-semibold">List Item </p>
+                            <p class="font-semibold ml-16">Quantity</p>
+                            <p class="font-semibold">Price Per Quantity</p>
+                        </div>
+                        <hr class="border-[#D9D9D9] w-[72%] mx-auto">
+                        <div class="flex justify-between w-9/12 mx-auto p-3 px-10 text-sm">
+                            <div>
+                            <p class="text-center pb-2" v-for="item in sale.itemName" :key="item">
+                                {{ item }}
+                            </p>
+                            </div>
+                            <div>
+                            <p  class="text-center pb-2 " v-for="qty in sale.quantity" :key="qty">
+                                {{ qty }}
+                            </p>
+                            </div>
+                            <div>
+                            <p  class="text-center pb-2" v-for="(price, priceIndex) in sale.itemPrice" :key="priceIndex">
+                                RM {{ (price / sale.quantity[priceIndex]).toFixed(2) }}
+                            </p>
+                            </div>
+                        </div>
+                        <hr class="flex justify-between border-[#D9D9D9] w-[72%] mx-auto">
+                        <div class="w-9/12 mx-auto p-3 px-10 text-sm" >
+                            <p class="text-right font-semibold">Total Quantity: {{ calculateTotalQuantity(sale.quantity) }}</p>
+                            <p class="text-right font-semibold">Payment Method: {{ sale.paymentMethod }}</p>
+                            <p class="text-right font-semibold">Total: RM {{ sale.price }}</p>
+                        </div>
+                    </div>
                 </div>
                 <div id="overlay" class="fixed z-40 w-screen h-screen inset-0 bg-gray-900 bg-opacity-50" v-bind:class="{'hidden': !isOpen}"></div>
                 <dialog class="w-7/12 mx-auto shadow-product rounded-2xl outline absolute top-44 z-50" v-bind:open="isOpen">
                     <form method="dialog" class="max-sm:text-xs max-md:text-sm">
+                        <div class="w-fit" @click="closeDialog">
+                            <p class=" text-sm text-red-600 cursor-pointer hover:text-black pb-5">
+                                <span class="text-sm">&lt;</span> 
+                                Kembali
+                            </p>
+                        </div>
                         <div class="flex justify-between p-5">
                             <p>Hasil Jualan</p>
                             <p>RM {{ parseFloat(totalSale.Online + totalSale.Tunai).toFixed(2) }}</p>
@@ -48,7 +105,7 @@ document.title="Tutup Jualan"
                             <p>Tarikh</p>
                             <p>{{ currentDate }}</p>
                         </div>
-                        <div class="w-max mx-auto">
+                        <div class="flex w-max mx-auto">
                             <button class="w-max bg-black text-white p-2 px-10 rounded-xl hover:bg-white hover:text-black hover:outline hover:outline-black " @click="closeSale">Laporan Jualan</button>
                         </div>
                     </form>
@@ -56,29 +113,38 @@ document.title="Tutup Jualan"
             </div>
         </div>
   </div>
+  <ToastMessageVue ref="toast"/>
 </template>
 <script>
 import axios from 'axios';
-import jsPDF from 'jspdf';
+import html2pdf from 'html2pdf.js';
+import ToastMessageVue from "../components/ToastMessage.vue";
 
+export default {
 
-export default
-{
+  components: {
+    ToastMessageVue,
+  },
     data()
     {
         return{
             isOpen:false,
+            tutupJualan:false,
             report:[],
+            total:null,
             allSale:[],
             totalSale:[],
             currentDate: '',
             updateReport:[],
+            workerData:[],
+            workerName:[],
             listItem:[],
             idItems:[],
             reportIdItems:[],
             price:[],
             quantity:[],
             item:[],
+            allSaleReport:[],
         }
     },
     mounted()
@@ -149,7 +215,7 @@ export default
         },
         closeSale()
         {
-
+            this.tutupJualan=true
             axios.get("http://localhost:3000/report")
             .then(response=>{
                 this.updateReport = response.data
@@ -167,6 +233,21 @@ export default
                 console.log('prices:', prices);
                 // console.log(idSale)
 
+                axios.get("http://localhost:3000/report/user/"+idAccounts)
+                .then(response=>
+                {
+                    this.workerData = response.data
+                    console.log('Worker data',this.workerData)
+
+                    this.workerData.forEach(data=>{
+                        const name = data.name
+
+                        this.workerName.push(name)
+                    })
+
+                })
+                .catch(error=>console.log(error))
+
             axios.get("http://localhost:3000/sale/" + idSale)
             .then(response => {
                 this.listItem = response.data;
@@ -180,7 +261,7 @@ export default
                 // console.log(listItemArray);
 
                 const idItemsArray = listItemArray.map(item => item.idItem);
-                const itemPrice = listItemArray.map(item => item.price);
+                const itemPrice = listItemArray.map(item => item.totalPrice);
                 const itemQuantity = listItemArray.map(item => item.quantity);
 
                 // console.log(idItemsArray);
@@ -203,18 +284,45 @@ export default
                 axios.get("http://localhost:3000/item/report/" + this.idItems)
                 .then(response => {
                     this.item = response.data;
-
-
                     console.log(this.item);
 
                     console.log('Number Sale:',this.updateReport[0].numberSale)
                     console.log('Total Sale',this.updateReport[0].saleRevenue)
+                    this.total=(this.updateReport[0].saleRevenue)
                     console.log('idAccounts:', idAccounts);
+                    console.log('Worker',this.workerName)
                     console.log('paymentMethods:', paymentMethods);
                     console.log('prices:', prices);
-                    console.log('all item id',this.reportIdItems)
-                    console.log('Price',this.price)
+                    console.log('All Item Id',this.reportIdItems)
+                    console.log('Price for each item in total',this.price)
                     console.log('Quantity',this.quantity)
+
+                    const idToNameMap = this.item.reduce((map, item) => {
+                    map[item.idItem] = item.name;
+                    return map;
+                    }, {});
+
+                    // Update array id item with item names
+                    const updatedIdItems = this.reportIdItems.map(ids => ids.map(id => idToNameMap[id]));
+
+                    console.log(updatedIdItems);
+
+                    for (let i = 0; i < this.price.length; i++) {
+
+                        const item = {
+                        workerName: this.workerName[i],
+                        paymentMethod: paymentMethods[i],
+                        price:prices[i],
+                        itemName : updatedIdItems[i],
+                        quantity : this.quantity[i],
+                        itemPrice: this.price[i]
+
+                        };
+                        // console.log(item)
+                        this.allSaleReport.push(item);
+                    }
+
+                    console.log(this.allSaleReport)
 
                     this.item = []
                     this.reportIdItems=[]
@@ -228,25 +336,40 @@ export default
             .catch(error=>console.log(error))
 
 
-            // const doc = new jsPDF();
-
-
-
-            // // Set font size and style
-            // doc.setFont('helvetica', 'bold');
-            // doc.setFontSize(8);
-            
-            // // Set text color
-            // doc.setTextColor('#FF0000');
-
-            // doc.text('Hello world! \nHazim Hensem', 10, 10);
-            // doc.save('Laporan Jualan.pdf');
-
             this.totalSale=[]
+            this.allSaleReport=[]
             this.isOpen=!this.isOpen
 
+            const message ='Perniagaan Berjaya Ditutup'
+            const status = 'Berjaya'
+            
+            this.$refs.toast.toast(message,status,'success')
 
+        },
+        formatArray(arr) {
+        return arr.join(', ');
+        },
+        calculateTotalQuantity(quantityArray) {
+            if (Array.isArray(quantityArray) && quantityArray.length > 0) {
+            return quantityArray.reduce((total, quantity) => total + quantity, 0);
+            }
+            return 0;
+        },
+        closeDialog()
+        {
+            this.isOpen=!this.isOpen
 
+            const message ='Perniagaan Masih Belum Ditutup'
+            const status = 'Amaran'
+            
+            this.$refs.toast.toast(message,status,'error')
+        },
+        downloadReport()
+        {
+            const report = document.getElementById('Report')
+            const fileName = `Laporan Jualan ${this.currentDate}`
+
+            html2pdf().from(report).save(fileName);
         }
     }
 }
